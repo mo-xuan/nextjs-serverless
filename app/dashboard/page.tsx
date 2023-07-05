784800a540ce838f7d577e17dc7882aa9106816f
mo-xuan <xuan.mo7@outlook.com>
2023-07-05 10:46:58 +0800

    feat: 初始化

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+export default function DashBoard() {
+  return(
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Link href="/">index</Link>
+    </main>
+  )
+}
